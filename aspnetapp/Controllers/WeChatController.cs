ecb8951524383951baa05932ae49d7ff18b1a540
振宇 杨 <978527977@qq.com>
2023-12-18 21:02:52 +0800

    添加获取Token接口

---
+++ aspnetapp/Controllers/WeChatController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aspnetapp.Controllers
+{
+    [Route("api/wechat")]
+    [ApiController]
+    public class WeChatController : Controller
+    {
+        [HttpPost("fs")]
+        public async Task<object> GetToken()
+        {
+            return await new HttpClient().GetAsync("http://api.weixin.qq.com/wxa/getwxadevinfo");
+        }
+    }
+    
+}
---
+++ aspnetapp/Migrations/20231207155640_2023120701.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace aspnetapp.Migrations
+{
+    public partial class _2023120701 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8");
+
+            migrationBuilder.CreateTable(
+                name: "Counters",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    count = table.Column<int>(type: "int", nullable: false),
+                    createdAt = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    updatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Counters", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8")
+                .Annotation("Relational:Collation", "utf8_general_ci");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Counters");
+        }
+    }
+}
